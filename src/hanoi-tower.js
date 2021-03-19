b35715a44406c99b0eaa1b4cf524337a752ad49a
pozner <poznerrr@gmail.com>
2021-03-20 00:10:03 +0300

    feat: add transform-array solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
- console.log('!disknum: ',disksNumber, '!!turnsspeed:  ',turnsSpeed);
   
   let turn = Math.pow(2,disksNumber) - 1;
   let second = Math.floor(turn / turnsSpeed * 3600);
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(! (typeof arr)== Array )
+  {throw 'Error';}
+  let resArr = [];
+  let itemDel=0;
+  let dbl = 0;
+  let discard =0;
+  for (let item of arr){
+    if(item == '--discard-next') {
+      discard = 1;
+      itemDel=0;
+    }  else 
+    if ( item == '--discard-prev') {
+      if (resArr.length>0 && itemDel == 0){
+      resArr.pop()
+      }
+      itemDel=1;
+    } else
+    if (item == '--double-next') {
+      dbl=1;
+      itemDel=0;
+      discard=0;
+    } else
+    if (item == '--double-prev') {
+      if (resArr.length > 0 && itemDel == 0) {
+      resArr.push(resArr[resArr.length-1]);
+      }
+      itemDel=0;
+      discard=0;
+      dbl=0;
+    } else  if (dbl == 1) {
+      if(itemDel==0) {
+      dbl=0;
+      resArr.push(item);
+      resArr.push(item);
+      } else
+      itemDel=0;
+    
+    } else if (discard==1) {
+      discard=0;
+      itemDel=1;
+    } 
+    else {resArr.push(item); itemDel=0;}
+  }
+  return resArr;
+  
 };
