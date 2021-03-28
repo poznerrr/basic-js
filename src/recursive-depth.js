91de9c08b1af56c1f569ca155b7ad8993dfa3f2e
pozner <poznerrr@gmail.com>
2021-03-28 21:56:27 +0300

    refactor: change array depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,12 +2,27 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr ) {
-   let deep = 1;
-    for (let i of arr) {
-      if (Array.isArray(i)){
-        deep=deep+this.calculateDepth(i);
+
+    var res = 1;
+    for(var i = 0; i < arr.length; ++i) {
+        if (Array.isArray(arr[i])) {
+        var subDepth = this.calculateDepth(arr[i]) + 1;
+        if (subDepth > res) {
+            res = subDepth;
+        }
       }
+    }
+    return res;
+}
+/*   let deep = 1;
+
+    for (let i=0; i<arr.length; i++) {
+      if (Array.isArray(arr[i])){
+        deep=deep+this.calculateDepth(arr[i]);
+
+      }
+      
    }
    return deep;
-  }
+  }*/
 };
