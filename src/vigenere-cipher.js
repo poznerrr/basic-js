d97b049e34cbd73a641ad4571a1cb06458267c4f
poznerrr <pozner@gmail.com>
2022-10-13 20:36:13 +0300

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    if (isDirect) {
+      this.isDirect = true;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, code) {
+    if (message == undefined || code == undefined) {
+      throw Error("Incorrect arguments!");
+    }
+    message = message.toUpperCase();
+    code = code.toUpperCase();
+    while (code.length < message.length) {
+      code = code + code;
+    }
+    let answer = [];
+    let codeCount = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) < 65 || message.charCodeAt(i) > 90) {
+        answer.push(message[i]);
+      }
+      else {
+        let symbol = (message.charCodeAt(i) + code.charCodeAt(codeCount) - 65);
+        codeCount += 1;
+        symbol > 90 ? symbol -= 26 : symbol;
+        answer.push(String.fromCharCode(symbol));
+      }
+    }
+    if (this.isDirect) { return answer.join(''); } else { return answer.reverse().join(''); }
+  }
+
+  decrypt(message, code) {
+    if (message == undefined || code == undefined) {
+      throw Error("Incorrect arguments!");
+    }
+    message = message.toUpperCase();
+    code = code.toUpperCase();
+    while (message.length > code.length) {
+      code += code;
+    }
+    let answer = [];
+    let codeCount = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) < 65 || message.charCodeAt(i) > 90) {
+        answer.push(message[i]);
+      }
+      else {
+        let symbol = (message.charCodeAt(i) - code.charCodeAt(codeCount) + 65);
+        codeCount += 1;
+        symbol < 65 ? symbol += 26 : symbol;
+        answer.push(String.fromCharCode(symbol));
+      }
+    }
+    if (this.isDirect) { return answer.join(''); } else { return answer.reverse().join(''); }
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
