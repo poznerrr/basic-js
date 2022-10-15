4f8a744f3126a733ea8cd93b782ee0faa8950adb
poznerrr <pozner@gmail.com>
2022-10-15 10:38:49 +0300

    feat: add seasons solituin without tricky moments

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (isNaN(Date.parse(date))) {
+    throw Error("Invalid date!");
+  }
+
+
+  switch (date.getMonth()) {
+    case 0:
+      return 'winter';
+    case 1:
+      return 'winter';
+    case 2:
+      return 'spring';
+    case 3:
+      return 'spring';
+    case 4:
+      return 'spring';
+    case 5:
+      return 'summer';
+    case 6:
+      return 'summer';
+    case 7:
+      return 'summer';
+    case 8:
+      return 'autumn';
+    case 9:
+      return 'autumn';
+    case 10:
+      return 'autumn';
+    case 11:
+      return 'winter';
+  }
 }
+
+
+
 
 module.exports = {
   getSeason
