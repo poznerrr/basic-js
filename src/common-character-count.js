10c189b22c2f169e17d2c294b7213f5177dd2d0d
poznerrr <pozner@gmail.com>
2022-10-15 10:52:42 +0300

    feat: add sort-by-height and comon and hanoi  character solutions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let set = new Set();
+  let answer = 0;
+  s1.split('').map(item => set.add(item));
+
+  for (let symbol of set) {
+    let local1 = 0;
+    let local2 = 0;
+
+    for (let i = 0; i < s1.length; i++) {
+      if (s1[i] == symbol) {
+        local1++
+      }
+    }
+
+    for (let i = 0; i < s2.length; i++) {
+      if (s2[i] == symbol) {
+        local2++
+      }
+    }
+
+    answer += Math.min(local1, local2);
+  }
+
+  return answer;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] == -1) { continue; }
+      else if (arr[j] < arr[i]) {
+        let tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = {
